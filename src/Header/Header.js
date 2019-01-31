1991fd256beda3233eb3e0503c445a9a05813941
Luke Allen <lukepatrickallen@gmail.com>
2019-01-31 09:42:51 -0800

    created initial file structure

---
+++ src/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div class="header">
+                <h3 class="header-title">Title</h3>
+            </div>
+        )
+    }
+}
+
+export default Header;
