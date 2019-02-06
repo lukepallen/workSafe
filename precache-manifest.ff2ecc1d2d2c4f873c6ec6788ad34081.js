adcb7aca65e080603024086946e387ffd86e6c20
Luke Allen <lukepatrickallen@gmail.com>
2019-02-05 20:37:24 -0800

    Updates

---
+++ precache-manifest.ff2ecc1d2d2c4f873c6ec6788ad34081.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9dc99b2540be4f926931",
+    "url": "/koolkids/static/js/runtime~main.9dc99b25.js"
+  },
+  {
+    "revision": "ed89401c4b9f38d3ea7f",
+    "url": "/koolkids/static/js/main.ed89401c.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/koolkids/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "ed89401c4b9f38d3ea7f",
+    "url": "/koolkids/static/css/main.fff3e4fd.chunk.css"
+  },
+  {
+    "revision": "b738afd90f2496b8d6136e5e2b575303",
+    "url": "/koolkids/index.html"
+  }
+];
