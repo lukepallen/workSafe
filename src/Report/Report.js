31ad1af77fa653a97477490bc029092ca73e20b3
Luke Allen <lukepatrickallen@gmail.com>
2019-03-08 13:51:27 -0800

    cleaned up console log

--- src/Report/Report.js
+++ src/Report/Report.js
@@ -70,13 +70,7 @@ class Report extends React.Component {
                     </div>
                 </div>
                 <div className="submit">
-                    <button className="btn" onClick={() => {
-                        this.firebase.add(this.reportInfo)
-                        this.firebase.getAll().then(data => {
-                            console.log(data)
-                            console.log(data.length)
-                        })
-                    }}>Submit</button>
+                    <button className="btn" onClick={() => this.firebase.add(this.reportInfo)}>Submit</button>
                 </div>
             </div>
         )
