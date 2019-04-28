893a95075684bac322cb327a5dc29ca2f4fce226
Luke Allen <lukepatrickallen@gmail.com>
2019-04-27 19:42:09 -0700

    switched from browserrouter to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
 import EmpApp from './EmployeeView/App';
 import HRApp from './HRView/App';
 import {ROUTES} from './constants';
